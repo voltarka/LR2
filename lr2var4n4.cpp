3eb965c14bff006bd9b4243506c9037e1948e95f
voltarka <70635056+voltarka@users.noreply.github.com>
2020-10-23 17:18:48 +0300

    Update lr2var4n4.cpp

--- lr2var4n4.cpp
+++ lr2var4n4.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int main(){
-    int n = 0;
-    int x;
-    double i;
-    double a;
-    int fact = 1;
-    int res = 1;
-    int b;
-    double c;
-    cout<< "Введите значение х"<< endl;
-cin>>x;
-i = cos(x);
-do
-{
-    for (b = 1; b <= fact; b++)
-    {
-        res = res * b;
-    }
-a = pow(-1,n)*pow(x,2*n)/res;
-n+=1;
-fact =2*n;
-c = abs(i -a);
+int fact(int &n){
+    int res,i;
+    res = 1;
+    for (i = 1; i <= n; i++)
+        res = res * i;
+    return res;
 }
-while ((c>= 0.01) && (c<= 0.000001));
-
-
-cout << "Приблизительно значение cos(x)="<< a<< endl;
-cout << "Количество итераций- "<< n<< endl;
-
+int main(){
+    double eps = 0, cos = 0, x = 0, a;
+    int n,iterations, factories;
+    bool flag = true;
+    while (flag){
+        std::cout << "Введите х от -1 до 1 "  ;
+        std::cin >> x;
+        if ((x > -1) || (x <= 1))
+            flag = false;
+        else
+            std::cout << "Неверное значение х" << std::endl;
+    }
+    for (int i = -2; i > -7; i--){
+        eps = pow(10,i);
+        n = 0; iterations = 0; a = 1, cos = 0;
+        while (abs(a) > eps){
+            factories = fact(n);
+            a = pow(-1,n)*pow(x,2*n)/factories;
+            cos += a;
+            n++;
+            iterations++;
+        }
+        std::cout << "cos x = " << cos << " с точностью = " <<
+        eps << ", посчитан с " << iterations << " количеством итераций" << std::endl;
+    }
     return 0;
 }
